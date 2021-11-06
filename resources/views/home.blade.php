4cb9ec1a40a0df7c4d5ab8c26ba8d420f228066b
quy <nguyen.van.quyb@sun-asterisk.com>
2021-11-06 09:06:39 +0700

    chore: change title in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 {{-- {{ dd(get_defined_vars()['__data']) }} --}}
 <div class="d-flex justify-content-end justify-content-sm-between">
-    <h4 class="d-none d-sm-flex align-items-center font-weight-bold">Posts List</h4>
+    <h4 class="d-none d-sm-flex align-items-center font-weight-bold">Viblo Posts List</h4>
     <nav class="navbar navbar-light bg-light pr-0">
         <form class="d-flex" action="{{ route('home')}}">
             <input class="form-control mr-2" name="search" type="search" placeholder="Search" aria-label="Search" value="{{request()->get('search')}}">
